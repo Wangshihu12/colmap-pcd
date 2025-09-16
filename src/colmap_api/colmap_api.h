735ac1d971b13e9eccd4e9169f9b4dff5ff20139
Wangshihu12 <852066173@qq.com>
2025-09-16 14:59:29 +0800

    删除多余的安装文件

--- src/colmap_api/colmap_api.h
+++ src/colmap_api/colmap_api.h
@@ -1,6 +1,16 @@
 #pragma once
-#include "export.h"
 #include <string>
+
+// windows dll export/import
+#ifdef _WIN32
+  #ifdef COLMAP_API_EXPORTS
+    #define COLMAP_API __declspec(dllexport)
+  #else
+    #define COLMAP_API __declspec(dllimport)
+  #endif
+#else
+  #define COLMAP_API
+#endif
 
 namespace colmap_api {
 
